edc2f45d8fc55c760be2ebaff33169613b798603
llMBQll <m.balabanow@gmail.com>
2025-08-27 17:33:15 +0200

    Create a central config file

---
+++ src/config.zig
@@ -0,0 +1,5 @@
+// Dump the bytecode after compiling each chunk
+pub const COMPTIME_DEBUG_TRACING = true;
+
+// Dump currently executing instruction and VM's stack contents
+pub const RUNTIME_DEBUG_TRACING = false;
